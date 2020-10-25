700fcfa7ad30ce2ea0eaf6758972af000698c5f6
James McMurray <jamesmcm03@gmail.com>
2020-10-25 14:18:36 +0100

    Fix genzip

--- genzip.sh
+++ genzip.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+cargo build --release --target=x86_64-unknown-linux-musl
 cp ./target/x86_64-unknown-linux-musl/release/bootstrap ./
 strip --strip-all ./bootstrap
 zip bootstrap.zip bootstrap
